1a74060b1d78f2a5185546da8affa70f5790c7bd
Mateusz Judziński <mateusz.judzinski@gmail.com>
2024-05-25 15:24:14 +0200

    Continous deployment code change

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, request
+import git
 
 app = Flask(__name__)
 
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./flask-portfolio')
+    origin = repo.remote.origin
+    repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 @app.route('/')
 def home():
     return render_template('index.html')
